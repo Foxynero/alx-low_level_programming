76198c8ac1f75d3823cf384f0a6129baa4e21daf
Foxynero <adugyamfifoster@gmail.com>
2022-10-16 10:07:24 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int q;
-	long int w;
-	long long int e;
-	float r;
-	char t;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("Size of an char: %1u byte(s)\n", (unsigned long)sizeof(t));
-	printf("Size of an int: %1u byte(s)\n", (unsigned long)size of(q));
-	printf("Size of a long int: %1u byte(s)\n", (unsigned long)size of (w));
-	printf("Size of a long long int: %1u byte(s)\n", (unsigned long)size of(e));
-	printf("Size of a float: %1u byte(s)\n", (unsigned long)size of(r));
+	printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %1u byte(s)\n", (unsigned long)size of(a));
+	printf("Size of a long int: %1u byte(s)\n", (unsigned long)size of (b));
+	printf("Size of a long long int: %1u byte(s)\n", (unsigned long)size of(c));
+	printf("Size of a float: %1u byte(s)\n", (unsigned long)size of(f));
 	return (0);
 }
