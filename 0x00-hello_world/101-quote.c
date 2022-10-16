9353a92b9bf52ba623a71d92f84c953c8e4b59cd
Foxynero <adugyamfifoster@gmail.com>
2022-10-16 10:32:37 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,9 +6,15 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
+
 int main(void)
+
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+
+		write(STDOUT_FILENO, "and that piece of art is useful
+
+							\" - Dora Korpar, 2015-10-19\n", 59);
+
+			return (1);
+
 }
