7c52e9443f42ee84b1350a9fb3681dc1b16fc830
Foxynero <adugyamfifoster@gmail.com>
2022-10-17 13:59:57 -0700

    variables if, else, and while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,17 @@ int main(void)
 {
 	int d;
 
-	for (d = '0'; d <= '9'; d++)
+	for (d = 0; d <= 9; d++)
 	{
-		putchar(d);
-		if (d != '9')
-		{
+		putchar((d % 10) + '0');
+		if (d == 9)
+			continue;
+		
 			putchar(',');
-			putchar(',');
-		}
+			putchar(' ');
+	
 	}
+
 	putchar('\n');
 
 	return (0);
