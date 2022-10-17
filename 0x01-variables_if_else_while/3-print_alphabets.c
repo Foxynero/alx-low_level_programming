3445ede79a1efacd11bc50c7da4d2696362364f3
Foxynero <adugyamfifoster@gmail.com>
2022-10-17 13:35:15 -0700

    variables if, else, and while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,7 +14,9 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	putchar('\n');
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	puthar('\n');
 
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time,h>
 
 /**
  * main - Entry point
@@ -10,9 +10,12 @@
 
 int main(void)
 {
+	int d;
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
 	putchar('\n');
 
