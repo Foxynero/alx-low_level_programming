ec2169f2a05a38b4f8eb8ebd23d3cfe8ed2a0d63
Foxynero <adugyamfifoster@gmail.com>
2022-10-19 07:57:55 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints_alphabets -  prints 10 times the alphabet in lowercase
+ * main - print_alphabet_x10 - prints the lowercase alphabet x10 with _putchar.
  *
  * Return: void
  */
