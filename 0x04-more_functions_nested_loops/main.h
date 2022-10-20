9f0166ea4a79cde605549d1c352c32a94ba9ef6e
Foxynero <adugyamfifoster@gmail.com>
2022-10-20 14:45:13 -0700

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,36 @@
+#ifndef _main_h_
+#define _main_h_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+
+int _isalpha(int c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int print_sign(int n);
+
+int _abs(int a);
+
+void print_to_98(int n);
+
+int print_last_digit(int a);
+
+void print_to_98(int n);
+
+int add(int a, int b);
+
+void jack_bauer(void);
+
+void print_times_table(int n);
+
+#endif /* _main_h_ */
