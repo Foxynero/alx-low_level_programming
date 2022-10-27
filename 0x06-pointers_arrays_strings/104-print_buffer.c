4b1144c198b44e5665282c7cf13e65a7aeb9d1e5
Foxynero <adugyamfifoster@gmail.com>
2022-10-27 04:06:39 -0700

    More on pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -5,27 +5,48 @@
  * print_buffer - prints a buffer
  *  @b: buffer to print
  *  @size: size of buffer
- *
- *  Return: void
  */
 
 void print_buffer(char *b, int size)
 {
-	int i;
+	int byte, index;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index++)
 		{
-			print_line(b, 9, i);
+			if ((index + byte) >= size)
+				printf("  ");
+
+			else
+				printf("%02x", *(b + index + byte));
+
+
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
 		}
-		else
+
+		for (index = 0; index < 10; index++)
 		{
-			print_line(b, size % 10 - 1, i);
+			if ((index + byte) >= size)
+				break;
+
+			else if (*(b + index + byte) >= 31 &&
+					*(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
+			else
+				printf(".");
 		}
-		putchar('\n');
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
 	}
-	if (size == 0)
-		putchar('\n');
+
+	if (size <= 0)
+		printf("\n");
 }
