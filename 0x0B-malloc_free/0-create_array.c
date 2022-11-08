2a54536dff0397301ecdaa3f1ceec79a8f60f191
Foxynero <adugyamfifoster@gmail.com>
2022-11-08 04:33:42 -0800

    Memory allocation and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it with a specific char.
+ * create_array - creates an array of chars
  * @size: an array of chars
  * @c: specific char to print
  *
