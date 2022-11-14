787c2a9108ac4b51cdc78e413ec01f088c409cb0
Foxynero <113718153+Foxynero@users.noreply.github.com>
2022-11-14 14:54:32 +0000

    Structures and type definitions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,30 +2,14 @@
 #define DOG_H
 
 /**
- * struct dog - struct for dog info
- * @name: type char
- * @age: float type
- * @owner: char type
- *
- *Description: structure to store dog info
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
-int _strlen(char *s);
-
-char *_strcpy(char *dest, char *src);
-
-#endif
+};
