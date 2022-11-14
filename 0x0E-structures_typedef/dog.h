f7e09312317b257f5c5eb28985a333790b867f2b
Foxynero <113718153+Foxynero@users.noreply.github.com>
2022-11-14 15:33:33 +0000

    Structures and type definitions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,10 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
- * struct dog - struct describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Description: store different data types
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
  */
 struct dog
 {
@@ -14,21 +12,13 @@ struct dog
 	float age;
 	char *owner;
 };
-
 /**
  * typedef dog_t - defines a new name for type struct dog
  */
-
 typedef struct dog dog_t;
-
 int _putchar(char c);
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
 #endif
