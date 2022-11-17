699126a8e579c8c20d5ee7d4dc066c9a5499556f
Foxynero <113718153+Foxynero@users.noreply.github.com>
2022-11-17 11:32:37 +0000

    Functions and nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,30 +3,32 @@
 /**
  * times_table - prints the 9 times table, starting with 0.
  *
- * Return: void
+ * Description: using for loops to create table and multiples
+ * Return: result
  */
-
 void times_table(void)
 {
-	int num, mult, prod;
+	int i, j, result;
 
-	for (num = 0; num <= 9; num++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		for (j = 1; j <= 9; j++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
+			result = i * j;
+			if (result < 10)
+			{
+				_putchar(',');
 				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
+				_putchar(' ');
+				_putchar(result + '0');
+			} else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
