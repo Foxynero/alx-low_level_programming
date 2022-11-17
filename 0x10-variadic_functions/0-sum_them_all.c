8e4bbab2e51f70165c3afec82017ab54119c385c
Foxynero <113718153+Foxynero@users.noreply.github.com>
2022-11-17 22:17:56 +0000

    Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,28 @@
-#include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
- * sum_them_all - variadic function to add number
- * @n: const unsigned int type
- * Return: sum of numbers
+ * sum_them_all - sum of all its parameters
+ * @n: number of arguements
+ * Return: sum of all parameters or 0
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int sum = 0;
+	va_list all_sum;
+	int total = 0;
 	unsigned int i;
-	va_list addlist;
-
-	va_start(addlist, n);
 
 	if (n == 0)
 		return (0);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(addlist, unsigned int);
-	}
-	va_end(addlist);
 
-	return (sum);
+	va_start(all_sum, n);
+
+	for (i = 0; i < n; i++)
+		total = total + va_arg(all_sum, unsigned int);
+
+	va_end(all_sum);
+	return (total);
+
 }
