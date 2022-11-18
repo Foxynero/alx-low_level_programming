ce01d2442988e0bbc91dee4ac68a66603404caeb
Foxynero <113718153+Foxynero@users.noreply.github.com>
2022-11-18 11:42:29 +0000

    Preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,5 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
-/*
- * File: 0-object_like_macro.h
- * Auth: Foster Adu-Gyamfi
- * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
- */
+#ifndef _HEADER_
+#define _HEADER_
 
 #define SIZE 1024
 
