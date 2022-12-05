2a50dfb46a9025b2c36464c39572f00b2e3822cf
Foxynero <adugyamfifoster@gmail.com>
2022-12-05 07:17:12 -0800

    File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,4 +16,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(const char *src, const char *dest);
 
-#endif
+#endif /* FILE_IO */
