72ad98603a83c413d47150a970b9baa8b3f19eaf
Foxynero <adugyamfifoster@gmail.com>
2023-01-28 04:00:49 -0800

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * add - add function
+ * @a: int type
+ * @b: int type
+ * Return: sum of two int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substract function
+ * @a: int type
+ * @b: int type
+ * Return: subtract two ints
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply function
+ * @a: int type
+ * @b: int type
+ * Return: multiply two ints
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two ints
+ * @a: int type
+ * @b: int type
+ * Return: divide two ints
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulos of two ints
+ * @a: int type
+ * @b: int type
+ * Return: modulus of two ints
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
